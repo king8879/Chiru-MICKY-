e4b4b1b8886975c35d34295f34f889e25c11af69
king8879 <k4507244@gmail.com>
2025-06-03 17:42:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "23664819")
+API_ID = os.environ.get("API_ID", "18579024")
 
-API_HASH = os.environ.get("API_HASH", "3853f97c662d5d08cee5f0d07361361e")
+API_HASH = os.environ.get("API_HASH", "124981da628d86e21ee492da77cd4037")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7694651611:AAFj-XVfUmXbZGK690521MdOlEdg8RygLvs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7243253595:AAH-UmQgZ0GfxMsdo8Eix6jsXzOmS9ZBTtM")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6886826162,))
+OWNER = int(os.environ.get("OWNER", 7726207129,))
 
-#LOG = -1002556158219
+#LOG = -1002510269081
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[8004315740,7744345481]
-    #for x in (os.environ.get("ADMINS", "8004315740,7744345481").split()):
+    #ADMINS=[7726207129]
+    #for x in (os.environ.get("ADMINS", "7726207129").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
